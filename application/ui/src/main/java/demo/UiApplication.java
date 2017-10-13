38e8a628ef0275e8c920551cc0145d3186307366
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2017-10-13 14:25:52 +0200

    Proxy version

--- application/ui/src/main/java/demo/UiApplication.java
+++ application/ui/src/main/java/demo/UiApplication.java
@@ -1,26 +1,28 @@
 package demo;
 
 import java.security.Principal;
-import java.util.Collections;
-import java.util.Map;
-
-import javax.servlet.http.HttpSession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableZuulProxy
 public class UiApplication {
+
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UiApplication.class, args);
@@ -29,7 +31,6 @@ public class UiApplication {
 	@Configuration
 	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 	protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			// @formatter:off
@@ -38,21 +39,12 @@ public class UiApplication {
 				.logout().and()
 				.authorizeRequests()
 					.antMatchers("/index.html", "/home.html", "/login.html", "/").permitAll()
-					.anyRequest().authenticated().and()
+					.anyRequest().authenticated()
+					.and()
 				.csrf()
 					.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
 			// @formatter:on
 		}
-	}
-
-	@RequestMapping("/user")
-	public Principal user(Principal user) {
-		return user;
-	}
-
-	@RequestMapping("/token")
-	public Map<String,String> token(HttpSession session) {
-		return Collections.singletonMap("token", session.getId());
 	}
 
 }
