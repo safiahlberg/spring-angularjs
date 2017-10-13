d26dd57b2f45f6ff774959d7ee5d59f4f2f359ef
Safi Ahlberg <10996839+safiahlberg@users.noreply.github.com>
2017-10-13 14:15:55 +0200

    Single version

--- application/src/test/java/demo/ApplicationTests.java
+++ application/src/test/java/demo/ApplicationTests.java
@@ -19,10 +19,35 @@ public class ApplicationTests {
 	@LocalServerPort
 	private int port;
 
+	private TestRestTemplate template = new TestRestTemplate();
+
 	@Test
-	public void homePageProtected() {
-		ResponseEntity<String> response = new TestRestTemplate().getForEntity("http://localhost:" + port + "/", String.class);
+	public void homePageLoads() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+	}
+
+	@Test
+	public void userEndpointProtected() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/user", String.class);
 		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+	}
+
+	@Test
+	public void resourceEndpointProtected() {
+		ResponseEntity<String> response = template.getForEntity("http://localhost:"
+				+ port + "/resource", String.class);
+		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+	}
+
+	@Test
+	public void loginSucceeds() {
+		TestRestTemplate template = new TestRestTemplate("user", "password");
+		ResponseEntity<String> response = template.getForEntity("http://localhost:" + port
+				+ "/user", String.class);
+		assertEquals(HttpStatus.OK, response.getStatusCode());
 	}
 
 }
